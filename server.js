7d79d12d28908649af3ffdb51ebe9e07d05abf2a
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-24 13:48:28 -0400

    added routes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const usersRouter = require('./routes/api/users');
+const authRouter = require('./routes/api/auth');
+const postsRouter = require('./routes/api/posts');
+const profileRouter = require('./routes/api/profile');
 const app = express();
 
 // Connect to Database
@@ -8,6 +12,12 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('API Running');
 });
+// Define routes
+app.use('/api/users', usersRouter);
+app.use('/api/posts', postsRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/auth', authRouter);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`DevConnector Started on PORT ${PORT}`);
