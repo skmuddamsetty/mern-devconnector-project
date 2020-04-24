3dd82d13afc2526c5d540ec5791e854644406f23
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-24 12:14:46 -0400

    Init Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`DevConnector Started on PORT ${PORT}`);
+});
