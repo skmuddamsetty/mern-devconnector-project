5216faec53981afa9f851974826e58586a895125
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-24 15:35:56 -0400

    completed node part

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const usersRouter = require('./routes/api/users');
-const authRouter = require('./routes/api/auth');
-const postsRouter = require('./routes/api/posts');
-const profileRouter = require('./routes/api/profile');
+const path = require('path');
+
 const app = express();
 
-// Connect to Database
+// Connect Database
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
-// Define routes
-app.use('/api/users', usersRouter);
-app.use('/api/posts', postsRouter);
-app.use('/api/profile', profileRouter);
-app.use('/api/auth', authRouter);
+// Init Middleware
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`DevConnector Started on PORT ${PORT}`);
-});
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
