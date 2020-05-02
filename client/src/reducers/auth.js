e99c9d13c31b1b99097c8b24985f486f07e9271d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-02 17:21:07 -0400

    completed setAuthToken - lecture 41

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,9 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -14,8 +19,11 @@ export default function (state = initialState, action) {
       localStorage.setItem('token', payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return { ...state, isAuthenticated: false, token: null, loading: false };
+    case USER_LOADED:
+      return { ...state, isAuthenticated: true, loading: false, user: payload };
     default:
       return state;
   }
