3324de7440a685d117cda69240882f42e00acc26
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-02 11:41:36 -0400

    added remove alert action

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -11,8 +11,5 @@ export const setAlert = (msg, alertType) => (dispatch) => {
       id,
     },
   });
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000);
 };
-
-// export const removeAlert = () => {
-//   type: REMOVE_ALERT;
-// };
