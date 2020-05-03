048cf13299e94003af301c608becee34479a5933
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-03 15:11:40 -0400

    closing this branch due to profile error

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = (props) => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
   return <div>Dashboard</div>;
 };
 
 Dashboard.propTypes = {};
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    profile: state.profile,
+    auth: state.auth,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getCurrentProfile: () => dispatch(getCurrentProfile()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
